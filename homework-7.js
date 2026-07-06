26ced331f304f772e76d87e96de2570a18a951da
Rustam Mugudinov <rustammugudinov@gmail.com>
2026-07-06 17:40:48 +0300

    fixed_function_and_tabulation

--- homework-7.js
+++ homework-7.js
@@ -1,41 +1,45 @@
-function forecastWeather(city, temperature) {
+function showWeather(city, temperature) {
     console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`);
 }
 
-forecastWeather('Москве', '26');
-forecastWeather('Лондоне', '25');
-forecastWeather('Париже', '30');
+showWeather('Москве', '26');
+showWeather('Лондоне', '25');
+showWeather('Париже', '30');
 
 
 const LIGHT_SPEED = 299792458;
-const testSpeed = 159792458;
 
-if (testSpeed > LIGHT_SPEED) {
-    console.log('Сверхсветовая скорость');
-} else if (testSpeed < LIGHT_SPEED) {
-    console.log('Субсветовая скорость');
-} else {
-    console.log('Скорость света');
+function testSpeed(speed) {
+    if (speed > LIGHT_SPEED) {
+        console.log('Сверхсветовая скорость');
+    } else if (speed < LIGHT_SPEED) {
+        console.log('Субсветовая скорость');
+    } else {
+        console.log('Скорость света');
+    }
 }
+
+testSpeed(333000000);
 
 
 const phone = 'Xiaomi 17 Pro';
 const phonePrice = 1000;
 
 const buyPhone = (budjet) => {
-    if(budjet >= phonePrice) {
-        return `${phone} приобретен. Спасибо за покупку.`;
+    if (budjet >= phonePrice) {
+        console.log(`${phone} приобретен. Спасибо за покупку.`);
+    } else {
+        const differencePrice = phonePrice - budjet;
+        console.log(`Вам не хватает ${differencePrice}$, пополните баланс.`);
     }
-    const differencePrice = phonePrice - budjet;
-    return `Вам не хватает ${differencePrice}$, пополните баланс.`;
 }
 
-console.log(buyPhone(1000));
+buyPhone(1000);
 
 
 
-function compChar(processor, ram, videocard) {
+function showCompChar(processor, ram, videocard) {
     console.log(`Процессор: ${processor}, ОЗУ: ${ram}, Видеокарта: ${videocard}`);
 }
 
-compChar('AMD Rizen 9', 'Kingston DDR5 512 Gb', 'NVIDIA Geforce 3070');
+showCompChar('AMD Rizen 9', 'Kingston DDR5 512 Gb', 'NVIDIA Geforce 3070');
